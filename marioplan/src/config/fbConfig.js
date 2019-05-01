3815130d02e4129917f9508d479c969629bc4597
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-05-01 10:11:03 +0300

    Setting up firebase

---
+++ marioplan/src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+//Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAem3yS2EHke7IAPAfgLXvIR2Aqhsc8dlA",
+    authDomain: "marioplan-436d3.firebaseapp.com",
+    databaseURL: "https://marioplan-436d3.firebaseio.com",
+    projectId: "marioplan-436d3",
+    storageBucket: "marioplan-436d3.appspot.com",
+    messagingSenderId: "465012603585"
+  };
+
+  firebase.initializeApp(config);
+  firebase.firestore().settings({ timestampsInSnapshots: true });
+
+  export default firebase;
+  
