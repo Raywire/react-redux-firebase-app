b1db3b7bf10444022a40f1c3c0903f678488158c
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-05-01 12:36:07 +0300

    Adding data to Firestore

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -7,13 +7,13 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { reduxFirestore, getFiretore } from 'redux-firestore';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import fbConfig from './config/fbConfig';
 
 const store = createStore(rootReducer,
     compose(
-    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFiretore })),
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
     reduxFirestore(fbConfig),
     reactReduxFirebase(fbConfig)
     )
--- marioplan/src/store/actions/projectActions.js
+++ marioplan/src/store/actions/projectActions.js
@@ -1,7 +1,19 @@
 const createProject = (project) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
-    dispatch({ type: 'CREATE_PROJECT', project });
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      // spread operator to spread the title and content
+      ...project,
+      authorFirstName: 'Ray',
+      authorLastName: 'Wire',
+      authorId: 12345,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({ type: 'CREATE_PROJECT', project });
+    }).catch((err) => {
+      dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+    })
   }
 };
 
