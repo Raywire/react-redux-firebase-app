e35c308328bbf00346de290e0dd26de0b47a7d40
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-05-01 15:04:04 +0300

    Logging users out

--- marioplan/src/components/layout/SignedInLinks.js
+++ marioplan/src/components/layout/SignedInLinks.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/authActions';
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
   return (
     <div>
       <ul className="right">
         <li><NavLink to="/create">New Project</NavLink></li>
-        <li><NavLink to="/">Log Out</NavLink></li>
+        <li><button onClick={props.signOut}>Log Out</button></li>
         <li><NavLink to="/" className="btn btn-floating pink lighten-1">RW</NavLink></li>
       </ul>
     </div>
   )
 }
 
-export default SignedInLinks;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(SignedInLinks);
--- marioplan/src/store/actions/authActions.js
+++ marioplan/src/store/actions/authActions.js
@@ -1,4 +1,4 @@
-const signIn = (credentials) => {
+export const signIn = (credentials) => {
   return (dispatch, getState, { getFirebase }) => {
     const firebase = getFirebase();
 
@@ -13,4 +13,14 @@ const signIn = (credentials) => {
   }
 }
 
-export default signIn;
+export const signOut = () => {
+  return (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase();
+
+    firebase.auth().signOut().then(() => {
+      dispatch({ type: 'SIGNOUT_SUCCESS' })
+    });
+  }
+}
+
+export default { signIn, signOut };
