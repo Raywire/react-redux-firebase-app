fb88a76f4b1fb2e46de9918fcb1c0243e699180c
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-05-01 09:37:29 +0300

    Adds dummy data

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+  projects: [
+    {id: '1', title: 'Help me find peach', content: 'blah blah blah' },
+    {id: '2', title: 'Collect all the stars', content: 'blah blah blah' },
+    {id: '3', title: 'Egg hunt with Yoshi', content: 'blah blah blah' },
+  ]
+}
 
 const projectReducer = (state = initState, action) => {
   return state;
